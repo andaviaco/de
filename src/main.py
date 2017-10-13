24eb8bdd8346a7659f667a1933bf8146e25aaf26
Andrés Ávila <andaviaco@gmail.com>
2017-10-12 21:19:23 -0500

    basic usage in main()

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
+import lib
+from de import DE
+
 
 def main():
-    pass
+    sphere = lib.sphereFn(2)
+    de = DE(5, 1, lib.sphere)
+    result_sphere = de.optimize()
+
+    print(f'Sphere MIN: x={result_sphere}')
 
 if __name__ == '__main__':
     main()
